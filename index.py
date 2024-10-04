09d315b5f600459adc778115ce371c3d086248dd
yamnor <yamnor@gmail.com>
2024-10-04 09:00:12 +0900

    simplify

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
-from bottle import route, run, template
+from bottle import route, run
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+@route('/')
+def index():
+  return '<b>Hello world</b>!'
 
-run(host='localhost', port=8080)
+run()
