3a8faa51d826b3468fcbb1822de34e74cf60e755
yamnor <yamnor@gmail.com>
2024-10-04 17:44:53 +0900

    test

--- index.py
+++ index.py
@@ -3,9 +3,9 @@ from bottle import Bottle, template
 app = Bottle()
 
 @app.route('/')
-def index():
-  return template('<b>Hello {{name}}</b>!', name='World')
+def root():
+  return template('<b>Hello World</b>!')
 
 @app.route('/hello/<name>')
-def index(name):
+def root(name):
   return template('<b>Hello {{name}}</b>!', name=name)
