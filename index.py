45bf53a7250efecab0ea8161d18ba95d9748dffd
yamnor <yamnor@gmail.com>
2024-10-04 09:12:32 +0900

    add route

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
-from bottle import Bottle
+from bottle import Bottle, template
 
 app = Bottle()
 
 @app.route('/')
 def index():
   return '<b>Hello world</b>!'
+
+@app.route('/hello/<name>')
+def index(name):
+  return template('<b>Hello {{name}} world</b>!', name=name)
